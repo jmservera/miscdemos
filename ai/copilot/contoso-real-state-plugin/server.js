e538e19c534e19aad837f93c504dd576d1a9e8bf
jmservera <8036360+jmservera@users.noreply.github.com>
2024-06-20 13:44:36 +0200

    contoso-real-state-plugin

---
+++ ai/copilot/contoso-real-state-plugin/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import getListings from "./get-listings.js"
+import path from "path";
+import cors from "cors";
+
+const app = express();
+app.use(cors({ origin: "https://www.bing.com" }));
+
+app.get("/openapi.yaml", (req, res) => {
+    res.sendFile(path.resolve() + "/openapi.yaml");
+  });
+
+app.get("/get-listings", (req, res) => {
+    const city = req.query.city;
+    const bedrooms = parseInt(req.query.bedrooms);
+    const bathrooms = parseInt(req.query.bathrooms);
+    const amenities = req.query.amenities;
+  
+    try {
+      const listings = getListings(city, bedrooms, bathrooms, amenities);
+      res.send(listings);
+    } catch (e) {
+      res.status(400).send({ error: e.message });
+    }
+  });
+
+  app.get("/.well-known/ai-plugin.json", (req, res) => {
+  res.sendFile(path.resolve() + "/ai-plugin.json");
+});
+
+app.get("/logo.png", (req, res) => {
+  res.sendFile(path.resolve() + "/logo.png");
+});
+
+app.listen(8080);
