92712e99474ebc1e7ff14515da88eabe3a71774a
jmservera <8036360+jmservera@users.noreply.github.com>
2025-11-17 11:14:43 +0100

    Enhance installation script for IoT Edge on Ubuntu 24.04 by adding WSL configuration and updating DNS settings

--- iotedge/ubuntu-24.04-install.sh
+++ iotedge/ubuntu-24.04-install.sh
@@ -13,6 +13,28 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+# Disable WSL auto generation of resolv.conf
+if [ -f /etc/wsl.conf ]; then
+    if grep -q "generateResolvConf" /etc/wsl.conf; then
+        echo "/etc/wsl.conf already contains generateResolvConf setting"
+    else
+        echo "Adding generateResolvConf setting to /etc/wsl.conf"
+        cat << EOF >> /etc/wsl.conf
+[network]
+generateResolvConf = false
+EOF
+    fi
+else
+    touch /etc/wsl.conf
+fi
+
+echo "Configuring /etc/resolv.conf"
+cat << EOF > /etc/resolv.conf
+nameserver 8.8.8.8
+nameserver 1.1.1.1
+nameserver 172.28.48.1
+EOF
+
 
 wget https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
@@ -22,7 +44,7 @@ apt-get update
 apt-get install moby-engine
 
 
-echo '{ "log-driver": "local" }' | tee /etc/docker/daemon.json
+echo '{ "log-driver": "local", "dns": ["1.1.1.1"] }' | tee /etc/docker/daemon.json
 systemctl restart docker
 
 apt-get install aziot-edge
